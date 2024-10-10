a9c0def885f424e70332e149c658f12377b2799c
Anand <anandsingh14442@gmail.com>
2024-10-10 22:56:35 +0530

    Solve ProductId error and working on Page/Products

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,40 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { ShopContext } from "../context/ShopContext";
 
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
+
+  const { productId } = useParams();
+
+  const { products } = useContext(ShopContext);
+  const [productData, setProdcutData] = useState(false);
+  const [image, setImage] = useState('')
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProdcutData(item)
+        setImage(item.image[0])
+        return null;
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products])
+
+
+  // console.log(productId)
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      <div>
+        
+      </div>
+    </div>
+  ) : <div className="opacity-0"></div>
 }
 
 export default Product
