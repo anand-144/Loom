87f027b018ffb3244132c614efbd9ab5506a6ab6
Anand <anandsingh14442@gmail.com>
2024-10-12 14:57:23 +0530

    product images

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,40 +1,56 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 
-
 const Product = () => {
-
   const { productId } = useParams();
-
   const { products } = useContext(ShopContext);
-  const [productData, setProdcutData] = useState(false);
-  const [image, setImage] = useState('')
+  const [productData, setProductData] = useState(null);  // Initialize as null
+  const [image, setImage] = useState('');
 
-  const fetchProductData = async () => {
-    products.map((item) => {
-      if (item._id === productId) {
-        setProdcutData(item)
-        setImage(item.image[0])
-        return null;
-      }
-    })
-  }
+  const fetchProductData = () => {
+    const product = products.find(item => item._id === productId);
+    if (product) {
+      setProductData(product);
+      setImage(product.image[0]);
+      console.log(product);
+    }
+  };
 
   useEffect(() => {
     fetchProductData();
-  }, [productId, products])
-
-
-  // console.log(productId)
+  }, [productId, products]);
 
   return productData ? (
     <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
-      <div>
-        
+      {/* ProductData */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Product Images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+                alt=""
+              />
+            ))}
+          </div>
+
+          <div className="w-full sm:w-[80%]">
+            <img src={image} alt="" className="w-full h-auto" />
+          </div>
+        </div>
+
+        {/* Product Info */}
+        {/* You can add more product details here, like name, price, etc. */}
       </div>
     </div>
-  ) : <div className="opacity-0"></div>
-}
+  ) : (
+    <div className="opacity-0"></div>
+  );
+};
 
-export default Product
+export default Product;
